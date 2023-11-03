9aca6f7a0cd4f174b029709a9995ebd3881fdffe
Elizizveta <74254392+Elizizveta@users.noreply.github.com>
2023-11-03 22:44:18 +0300

    Update Task_3.py

--- Лабораторная работа 1/Task_3.py
+++ Лабораторная работа 1/Task_3.py
@@ -1,4 +1,8 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-book_mb = 100*50*25*4/1024/1024
+str = 100
+stroki = 50
+simvoli = 25
+mesto = 4 # в байтах
+book_mb = str*stroki*simvoli*mesto/1024/1024
 disk = 1.44
 print("Количество книг, помещающихся на дискету:", int(disk//book_mb))
